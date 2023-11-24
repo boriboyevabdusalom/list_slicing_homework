85d23c0a8f890b64809164fd05ac50a89ab95cae
abdusalom <boriboyevabdusalom@gmail.com>
2023-11-24 09:44:39 +0500

    7

--- Slicing_list07.py
+++ Slicing_list07.py
@@ -7,4 +7,6 @@ def main(list1,n):
     Returns:
         list: return answer.
     """
-    return
+    return list1[::n]
+print(main(['a', 'b', 'c', 'd', 'e', 'f'],-1))
+print(main(['a', 1, 'b', 2, 'c', 3, 'd', 4],2))
